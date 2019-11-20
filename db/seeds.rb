bf47ec1ddefbdaaf486b29c21a5702b8dfcbd757
Alain Jolicoeur <aljcdev@gmail.com>
2019-11-20 09:40:43 -0500

    Implemented custom scopes for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,19 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
+3.times do |topic|
+	Topic.create!(
+		title: "Topic #{topic}"
+		)
+end
+
+puts "3 topics created"
+
 10.times do |blog|
 	Blog.create!(
 		title: "My blog post #{blog}",
-		body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet."
+		body: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.",
+		topic_id: Topic.last.id
 		)
 end
 
@@ -19,14 +28,23 @@ end
 
 puts "5 skills created"
 
-9.times do |portfolio_item|
+8.times do |portfolio_item|
 	Portfolio.create!(
-		title: "Portfolio title is #{portfolio_item}",
-		subtitle: "#{portfolio_item}th subtitle",
-		body: "blah blah   #{portfolio_item}  blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ",
+		title: "Portfolio title: #{portfolio_item}",
+		subtitle: "Ruby on Rails",
+		body: "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ",
 		main_image: "http://placehold.it/600x400",
 		thumb_image: "http://placehold.it/350x200"
 		)
 end
 
+1.times do |portfolio_item|
+	Portfolio.create!(
+		title: "Portfolio title: #{portfolio_item}",
+		subtitle: "Angular",
+		body: "blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah ",
+		main_image: "http://placehold.it/600x400",
+		thumb_image: "http://placehold.it/350x200"
+		)
+end
 puts "9 portfolio items created"
