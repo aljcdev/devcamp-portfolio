bcc456f46da78390aabfe388ca40a3d80179042d
Alain Jolicoeur <aljcdev@gmail.com>
2019-12-13 09:51:36 -0500

    Updated portfolio db to include attr for position

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,12 +2,17 @@ class Portfolio < ApplicationRecord
 		has_many :technologies
 		accepts_nested_attributes_for :technologies, 
 																	reject_if: lambda { |attrs| attrs['name'].blank?}
+		
 		include Placeholder
 		validates_presence_of :title, :body
 
 		def self.angular
 			where(subtitle: 'Angular')
 		end
+
+		def self.by_position
+			order("position ASC")
+		end		
 
 		scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
 
@@ -17,5 +22,4 @@ class Portfolio < ApplicationRecord
 			self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
 			self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
 		end
-
 end
