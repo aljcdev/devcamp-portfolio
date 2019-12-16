7e13c4a5818ce54ed58f5cd811eeae548ff994bb
Alain Jolicoeur <aljcdev@gmail.com>
2019-12-16 10:27:59 -0500

    Stuck in the middle of lecture 162

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,13 @@
 // that code so it'll be compiled.
 // live lines should be 6 7 8 9 11 14 15 16 17
 require("@rails/ujs").start()
+
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("packs/html.sortable")
+
+require("packs/portfolios")
 
 import "bootstrap"
 // import "../stylesheets/application"
@@ -16,7 +19,7 @@ document.addEventListener("turbolinks:load", () => {
 	$('[data-toggle="tooltip"]').tooltip()
 	$('[data-toggle="popover"]').popover()
 })
-
+alert("Hey there application.js");
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
