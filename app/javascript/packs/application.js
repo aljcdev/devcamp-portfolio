53de38d3759973e2584923d36a53262145134945
Alain Jolicoeur <aljcdev@gmail.com>
2019-12-17 09:06:46 -0500

    Implemented initial file uploading system via carrierwave for portfolio items

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,7 +19,6 @@ document.addEventListener("turbolinks:load", () => {
 	$('[data-toggle="tooltip"]').tooltip()
 	$('[data-toggle="popover"]').popover()
 })
-alert("Hey there application.js");
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
