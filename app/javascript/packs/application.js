e081999397e3067b9b6ae2c74e8d6d53f245c2f6
Alain Jolicoeur <aljcdev@gmail.com>
2019-12-18 15:59:26 -0500

    No change

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,9 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+
 require("packs/html.sortable")
+
 
 require("packs/portfolios")
 
